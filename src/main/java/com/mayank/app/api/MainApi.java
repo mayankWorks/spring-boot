39b8b97964494e93a4e42b552cafa9288db31dcd
mayankWorks <mayank.shah88@gmail.com>
2016-03-11 08:17:21 +0530

    fix

--- src/main/java/com/mayank/app/api/MainApi.java
+++ src/main/java/com/mayank/app/api/MainApi.java
@@ -3,30 +3,27 @@
  */
 package com.mayank.app.api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Maverick
  *
  */
-
-
 @RestController
 //@RequestMapping("/")
-@SpringBootApplication
+//@SpringBootApplication
 public class MainApi {
 
-	@RequestMapping("/hello")
-	public String hello(){
+	@RequestMapping(value="/hello")
+	public @ResponseBody String hello(){
 		
 		return "hello dear !!";
 	}
 	
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		SpringApplication.run(MainApi.class, args);
-	}
+	}*/
 
 }
---
+++ src/main/java/com/mayank/app/application/Application.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mayank.app.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+
+/**
+ * @author Maverick
+ *
+ */
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class})
+//@ComponentScan
+//@EnableAutoConfiguration
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class, args);
+	}
+
+}
