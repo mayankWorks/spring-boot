169c14c3521ce1a25cdea83c3b0c6ddb47baf739
mayankWorks <mayank.shah88@gmail.com>
2016-03-20 12:23:44 +0530

    h

--- src/main/java/com/mayank/app/application/Application.java
+++ src/main/java/com/mayank/app/application/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  * @author Maverick
  *
  */
+ 
 @SpringBootApplication()//exclude = { ErrorMvcAutoConfiguration.class}
 //@ComponentScan
 //@EnableAutoConfiguration
